34c6dd1dce1d67c5b01a236c4a24eff10a16d5ec
Prajwalkanadamane <prajwal.kanadamane450@gmail.com>
2026-06-19 22:13:57 +0530

    DP started

---
+++ Dynamic_Programming/firstOne.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fibonacci(int n, vector<int>& dp){
+
+    if (n<=1){
+        return n;
+    }
+    if (dp[n] != -1){
+        return dp[n];
+    }
+
+    return dp[n] = fibonacci(n-1, dp) + fibonacci(n-2, dp);
+
+}
+
+int main(){
+
+    int n= 5;
+    vector<int> dp(n+1, -1);
+
+    int ans = fibonacci(n, dp);
+    cout << ans;
+
+    return 0;
+}
