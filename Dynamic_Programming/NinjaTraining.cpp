27088df544066974257624d8e07cd348964548d8
Prajwalkanadamane <prajwal.kanadamane450@gmail.com>
2026-07-05 21:58:57 +0530

    NinjaTraining half done

---
+++ Dynamic_Programming/NinjaTraining.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int ninjaTraining(int day, int last, vector<vector<int>>& points){
+
+    if (day == 0){
+
+        int maxi = 0;
+        for (int task = 0; task < 3; task++){
+            if (task != last) maxi = max(maxi, points[0][task]);
+        }
+        return maxi;
+    }
+
+    int maxi = 0;
+
+    for (int task = 0; task < 3; task++){
+        if (task != last){
+            int point = points[day][task] + ninjaTraining(day-1, task, points);
+            maxi = max(maxi, point);
+        }
+    }
+    return maxi;
+
+}
+
+int main(){
+
+    vector<vector<int>> points = {{10, 11, 19},{4, 13, 7},{1, 8, 13}};  
+
+    cout << "Maximum points obtaining path is: " << ninjaTraining(points.size()-1, 3, points) << endl;                            
+    
+    return 0;
+}
