4313aa8c224314aa71a8ce3ff72b0d1d293ec18f
Prajwalkanadamane <prajwal.kanadamane450@gmail.com>
2026-07-16 22:39:10 +0530

    MinimumInsertionstoMakeStringPalindrome.cpp

---
+++ Dynamic_Programming/MinimumInsertionstoMakeStringPalindrome.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+
+int f3(string& a, string& b){
+
+    int n = a.size();
+    int m = b.size();
+    vector<vector<int>> DP(n+1, vector<int>(m+1, 0));
+
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=m; j++){
+            if (a[i-1] == b[j-1]){
+                DP[i][j] = 1 + DP[i-1][j-1];
+            }else
+            DP[i][j] = max (DP[i-1][j], DP[i][j-1]);
+        }
+    }
+    return DP[n][m];
+}
+
+
+int main(){
+    string a = "agbcba";   // agbcbga
+    string b = a;
+    reverse(b.begin(), b.end());
+    int ans = f3 (a, b);
+
+    cout << "Minimum Insertions to Make String Palindrome: " << a.size() - ans << endl;
+
+    return 0;
+}
