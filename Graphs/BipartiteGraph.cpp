204b4aeed64b1bdaff3292e30217d80d23d033cb
Prajwalkanadamane <prajwal.kanadamane450@gmail.com>
2026-08-10 22:18:53 +0530

    is graph bipartite

---
+++ Graphs/BipartiteGraph.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool f(int idx, int clr, vector<vector<int>>& edges, vector<int>& color){
+
+    color[idx] = clr;
+
+    for (auto neigh : edges[idx]){
+        if (color[neigh] == -1){
+            f(neigh, !clr, edges, color);
+        }else if(color[neigh] = color[idx]) return false;
+    }
+    return true;
+
+}
+
+int main(){
+
+    vector<vector<int>> edges = {{1,2,3}, {0,2}, {0,1,3}, {0,2}};
+    
+    vector<int> color(edges.size(), -1);
+    
+        for (int i=0; i<edges.size(); i++){
+            if (color[i] == -1){
+                if(!f(i, 0, edges, color)) {
+                    cout << "Not a Bipartite" << endl;
+                    return 0;
+                }
+            }
+        }
+
+        cout << "Graph is a Bipartite" << endl;
+        return 0;
+
+}
